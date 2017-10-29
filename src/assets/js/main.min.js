a28fa77e691eeb11297fd3a8dea3c28cfda06c7c
poprocki <eugeniusz@iguanastudio.pl>
2017-10-29 11:08:54 +0100

    header, top, nav, main title, dropdowns, buttons

--- src/assets/js/main.min.js
+++ src/assets/js/main.min.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+    $('.has-dropdown').on('click', function(){
+        $('.has-dropdown').removeClass('is-dropdown-open');
+        $(this).addClass('is-dropdown-open');
+    });
+
+    // body events
+    $('body').on('click touchend', function(e){
+        if(!$(e.target).closest('.has-dropdown').length) {
+            $('.has-dropdown').removeClass('is-dropdown-open');
+        }
+    });
 });
 
 
